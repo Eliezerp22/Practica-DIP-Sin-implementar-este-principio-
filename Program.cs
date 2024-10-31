c503b5470b4cd9515c0e4cfaa884aea393c49861
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-10-31 16:52:40 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_DIP_Sin_implementar_este_principio_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CustomerRepository
+    {
+        public Customer GetCustomerById(int id)
+        {
+            return new Customer { Id = id, Name = "Cliente de ejemplo" };
+        }
+    }
+
+    public class CustomerService
+    {
+        private readonly CustomerRepository _customerRepository;
+
+        public CustomerService()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return _customerRepository.GetCustomerById(id);
+        }
+    }
+
+}
+
+
